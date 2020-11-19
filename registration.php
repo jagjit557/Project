b009d204d6df9025ff408a794059afffe23b52ff
jagjit557 <52268448+jagjit557@users.noreply.github.com>
2020-11-19 09:29:24 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,32 +1,30 @@
-<?php
- 	session_start();
- 	header('location:login.php');
- 	
- 	$con = mysqli_connect('localhost','jagjit','dd@123');
- 	if($con){
- 		echo"connection success";
- 	}
- 	else{
- 		echo"connection NOT sucess";
- 	}
- 	
- 	mysqli_select_db($con, 'demo');
- 	
- 	$name=$_POST['email'];
- 	$password=$_POST['password'];
- 	
- 	$q="select * from signin where name = '$name' && password = '$password' ";
- 	
- 	$result = mysqli_query($con,$q);
- 	
- 	$num = mysqli_num_rows($result);
- 	
- 	if($num==1){
- 		echo"Duplicate data";
- 	}
- 	else{
- 		$qy="insert into signin(name,password) values ('$name','$password')";
- 		mysqli_query($con,$qy);
- 	}
- 	
- ?>
+ <?php
+  
+  $con = mysqli_connect('localhost','jagjit','dd@123');
+
+    // get details
+    
+    $firstname = mysqli_real_escape_string($conn, $_POST["firstname"]);
+    $lastname = mysqli_real_escape_string($conn, $_POST["lastname"]);
+    $email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $password = mysqli_real_escape_string($conn, $_POST["password"]);
+    
+    $hashed_password = md5($password);
+    $query1 = "SELECT id FROM users WHERE email = '$email' AND password = '$hashed_password'";
+    $query1 = "INSERT INTO users (name, email, password, contact, city, address) VALUES ('$name', $lastname, '$email', '$hashed_password')";
+
+    // check if already registered user
+    $result = mysqli_query($conn, $query1);
+    if(mysqli_num_rows($result) > 0){
+        // email already exists
+        echo "Email id already exists. Try another";
+    }else{
+        // perform query operation
+        $result = mysqli_query($conn, $query2);
+        $_SESSION["email_id"] = $email;
+        $_SESSION["id"] = mysqli_insert_id($conn);
+
+       
+        header("location: login.php");
+    }
+?>
