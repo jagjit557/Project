cfe74223c7e26fec72a9749c780d9e6af879d3d6
Jagjit Singh <52268448+jagjit557@users.noreply.github.com>
2020-11-20 16:19:52 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<?php
+    $conn = mysqli_connect("localhost", "root", "", "store") or die(mysqli_error($conn));
+    
+    if(!isset($_SESSION)){
+        session_start();
+    }
+?>
 <!DOCTYPE html>
 <head>
 <title>LogIn Page</title>
